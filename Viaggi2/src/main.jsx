24c3d424041290fedac80b0c72b00e0db8eaa96c
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2023-03-30 08:32:33 +0200

    Esercitazione 29-03-2023

--- Viaggi2/src/main.jsx
+++ Viaggi2/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import MainLayout from "./layouts/mainLayout";
+import Activities from "./pages/activities";
+import Activity from "./pages/activities/id";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,19 +15,15 @@ import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="about" element={<About />}>
-      <Route path="me" element={<h1>Sono io!</h1>} />
-      <Route path="her" element={<h1>È lei!</h1>} />
-      <Route path=":id" element={<h1>Tutti gli altri percorsi</h1>} />
+    <Route element={<MainLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="activities" element={<Activities />} />
+      <Route path="activities/:id" element={<Activity />} />
+      <Route path="city/:id" element={<h1>Activity</h1>} />
     </Route>
-    <Route path="/activity/:id" element={<h1>activity</h1>} />
-    <Route path="/city/:id" element={<h1>city</h1>} />
-  </Route>
-)
+  )
 );
- 
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
